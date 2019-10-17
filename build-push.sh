1f24ec592c7d95ebaf4749c9842d4c13e432309d
André Santos <andre.cunha@vilt-group.com>
2019-10-17 17:43:42 +0100

    Change version

--- build-push.sh
+++ build-push.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-docker build --tag andrerfcsantos/kube-letsencrypt:dryrun-0.1.0 .
-docker push andrerfcsantos/kube-letsencrypt:dryrun-0.1.0
+docker build --tag andrerfcsantos/kube-letsencrypt:dryrun-0.1.1 .
+docker push andrerfcsantos/kube-letsencrypt:dryrun-0.1.1
 
