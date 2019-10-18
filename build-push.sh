ab83cfa5c292786e57ac94d02512e1ac3ef50cf1
André Santos <andre.cunha@vilt-group.com>
2019-10-18 15:03:55 +0100

    Make curl less verbose

--- build-push.sh
+++ build-push.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-docker build --tag andrerfcsantos/kube-letsencrypt:dryrun-0.1.15 .
-docker push andrerfcsantos/kube-letsencrypt:dryrun-0.1.15
+docker build --tag andrerfcsantos/kube-letsencrypt:dryrun-0.1.16 .
+docker push andrerfcsantos/kube-letsencrypt:dryrun-0.1.16
